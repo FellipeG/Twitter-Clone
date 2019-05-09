a6d13c5477dac6b4fbdf3718fcffe41fdd81aad6
FellipeG <fellipegarciasrj@yahoo.com.br>
2019-05-09 12:57:06 -0300

    Blocking the registration of users already registered

--- registra_usuario.php
+++ registra_usuario.php
@@ -9,7 +9,39 @@
     $objDb = new Db();
     $conn = $objDb->conecta_mysql();
 
-    $sql = "INSERT INTO usuario (usuario, email, senha) VALUES ('$usuario','$email','$senha')";
+    $sql = "SELECT usuario FROM usuarios WHERE usuario='$usuario'";
+    $exec = mysqli_query($conn, $sql);
+    if ($exec) {
+
+        $dados = $exec->fetch_array();
+        if(isset($dados['usuario'])) {
+            echo 'Esse nome de usuário já existe, utilize outro.';
+        } else {
+            echo 'Usuário ok.';
+        }
+
+    } else {
+        echo 'Erro de SQL ao localizar registro de usuário';
+    }
+
+    $sql = "SELECT email FROM usuarios WHERE email='$email'";
+    $exec = mysqli_query($conn, $sql);
+    if ($exec) {
+
+        $dados = $exec->fetch_array();
+        if(isset($dados['email'])) {
+            echo 'Esse email já existe, utilize outro.';
+        } else {
+            echo 'Email ok.';
+        }
+
+    } else {
+        echo 'Erro de SQL ao localizar registro de email';
+    }
+
+    die();
+
+    $sql = "INSERT INTO usuarios (usuario, email, senha) VALUES ('$usuario','$email','$senha')";
     echo (mysqli_query($conn, $sql)) ? 'Usuário inserido com sucesso' : 'Erro ao inserir usuário';
 
 ?>
