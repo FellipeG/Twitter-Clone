127eef22f46ee9fb62983e32c3313ed6b83f0844
FellipeG <fellipegarciasrj@yahoo.com.br>
2019-05-09 13:40:31 -0300

    Finished blocking users already registered

--- registra_usuario.php
+++ registra_usuario.php
@@ -9,16 +9,17 @@
     $objDb = new Db();
     $conn = $objDb->conecta_mysql();
 
+    $usuario_existe = false;
+    $email_existe = false;
+
     $sql = "SELECT usuario FROM usuarios WHERE usuario='$usuario'";
     $exec = mysqli_query($conn, $sql);
     if ($exec) {
 
         $dados = $exec->fetch_array();
         if(isset($dados['usuario'])) {
-            echo 'Esse nome de usuário já existe, utilize outro.';
-        } else {
-            echo 'Usuário ok.';
-        }
+            $usuario_existe = true;
+        } 
 
     } else {
         echo 'Erro de SQL ao localizar registro de usuário';
@@ -30,16 +31,28 @@
 
         $dados = $exec->fetch_array();
         if(isset($dados['email'])) {
-            echo 'Esse email já existe, utilize outro.';
-        } else {
-            echo 'Email ok.';
+            $email_existe = true;
         }
 
     } else {
         echo 'Erro de SQL ao localizar registro de email';
     }
 
-    die();
+    if ($usuario_existe || $email_existe) {
+
+        $retorno_get = '';
+
+        if ($usuario_existe) {
+            $retorno_get .= 'erro_usuario=1&';
+        }
+
+        if ($email_existe) {
+            $retorno_get .= 'erro_email=1&';
+        }
+
+        header('Location: inscrevase.php?'.$retorno_get);
+        die();
+    }
 
     $sql = "INSERT INTO usuarios (usuario, email, senha) VALUES ('$usuario','$email','$senha')";
     echo (mysqli_query($conn, $sql)) ? 'Usuário inserido com sucesso' : 'Erro ao inserir usuário';
