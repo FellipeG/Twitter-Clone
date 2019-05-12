13b7a4b7243b31ff5482e661a3d375fdfdc3af14
FellipeG <fellipegarciasrj@yahoo.com.br>
2019-05-11 23:18:56 -0300

    Controlling the display of the following buttons and not following - Final

--- get_pessoas.php
+++ get_pessoas.php
@@ -16,16 +16,28 @@
     $nome_pessoa = (isset($_POST['nome_pessoa'])) ? $_POST['nome_pessoa'] : '';
 
     if (!empty($nome_pessoa)) {
-        $sql = "SELECT * FROM usuarios WHERE usuario LIKE '%$nome_pessoa%' AND id <> $_SESSION[id]";
+
+        $sql = "SELECT u.*, S.* FROM usuarios u LEFT JOIN seguidores s
+                ON (s.id_seguidor=$_SESSION[id] AND u.id = s.id_seguido)
+                WHERE u.usuario LIKE '%$nome_pessoa%' 
+                AND u.id <> $_SESSION[id] ORDER BY s.id_seguidor DESC";
+
         $query = mysqli_query($conn, $sql);
         if ($query) {
             if ($query->num_rows > 0) {
                 while ($linha = $query->fetch_array(MYSQLI_ASSOC)){
+                    if ((isset($linha['id_seguido'])) && (!empty($linha['id_seguido']))) {
+                        $btn_seguir = "display: none;";
+                        $btn_deixar_seguir = "display: inline-block;;";
+                    } else {
+                        $btn_seguir = "display: inline-block;;";
+                        $btn_deixar_seguir = "display: none;";
+                    }
                     echo '<a href="#" class="list-group-item">';
                     echo "<strong>$linha[usuario]</strong> <small> - $linha[email]</small>";
                     echo '<p class="list-group-item-text pull-right">';
-                    echo "<button id='btn_seguir$linha[id]' class='btn btn-default btn_seguir' data-id_usuario='$linha[id]'>Seguir</button>";
-                    echo "<button style='display: none;' id='btn_deixarDeSeguir$linha[id]' class='btn btn-primary btn_deixarDeSeguir' data-id_usuario='$linha[id]'>Deixar de seguir</button>";
+                    echo "<button style='$btn_seguir' id='btn_seguir$linha[id]' class='btn btn-default btn_seguir' data-id_usuario='$linha[id]'>Seguir</button>";
+                    echo "<button style='$btn_deixar_seguir' id='btn_deixarDeSeguir$linha[id]' class='btn btn-primary btn_deixarDeSeguir' data-id_usuario='$linha[id]'>Deixar de seguir</button>";
                     echo '</p>';
                     echo '<div class="clearfix"></div>';
                     echo '</a>';
